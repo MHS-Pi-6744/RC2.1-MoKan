a48d4c1c4a3076ba9817799500aef15f41a10fa3
Pete <162204327+UnderTheBedStudios@users.noreply.github.com>
2026-03-07 13:09:30 -0600

    led

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -6,11 +6,29 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LEDSubsystem extends SubsystemBase{
     public enum Pattern {
-        RRP(-.99),
-        RPP(-.97),
-        ROP(-.95),
-        RLP(-.93),
-        RFP(-.91)
+        Rainbow_Rainbow(-.99),
+        Rainbow_Party(-.97),
+        Rainbow_Ocean(-.95),
+        Rainbow_Lava(-.93),
+        Rainbow_Forest(-.91),
+        Rainbow_Glitter(-0.89),
+        Confetti(0.87),
+        Shot_Red(-0.85),
+        Shot_Blue(-0.83),
+        Shot_White(-0.81),
+        Sinelon_Rainbow(-0.79),
+        Sinelon_Party(-0.77),
+        Sinelon_Ocean(-0.75),
+        Sinelon_Lava(-0.73),
+        Sinelon_Forest(-0.71),
+        BPM_Rainbow(-0.69),
+        BPM_Party(-0.67),
+        BPM_Ocean(-0.65),
+        BPM_Lava(-0.63),
+        BPM_Forest(-0.61),
+        Fire_Medium(-0.59),
+        Fire_Large(-0.57),
+        
 
         private double value;
 
