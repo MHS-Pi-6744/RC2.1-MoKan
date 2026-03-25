b0858f02e64918dc7f850a21a23bc4680fb5895c
Pete <162204327+UnderTheBedStudios@users.noreply.github.com>
2026-03-25 15:21:14 -0500

    SmartCurrentLimit changed for swerve drive motors

--- src/main/java/frc/robot/Configs.java
+++ src/main/java/frc/robot/Configs.java
@@ -25,7 +25,7 @@ public final class Configs {
       double nominalVoltage = 12.0;
       double drivingVelocityFeedForward = nominalVoltage / ModuleConstants.kDriveWheelFreeSpeedRps;
 
-      drivingConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(50);
+      drivingConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(40);
       drivingConfig
           .encoder
           .positionConversionFactor(drivingFactor) // meters
