7810499a78b60e52022c1ba1f14263128bc6d03e
MattheDev53 <hashcube.dev@proton.me>
2026-03-25 18:05:32 -0500

    correct kDrivingMotorPinionTeeth

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -73,7 +73,7 @@ public final class Constants {
     public static final double kIntakeSpeed = 0.6; // Intake speed Units are percentage
 
     public static final class PivotSetPoints {
-      
+
       public static final double kStartPosition = 7; // to stay away from zero encoder reading
       public static final double kMiddlePosition = 40;
       public static final double kEndPosition = 103;
@@ -175,7 +175,7 @@ public final class Constants {
     // The MAXSwerve module can be configured with one of three pinion gears: 12T,
     // 13T, or 14T. This changes the drive speed of the module (a pinion gear with
     // more teeth will result in a robot that drives faster).
-    public static final int kDrivingMotorPinionTeeth = 12;
+    public static final int kDrivingMotorPinionTeeth = 14;
 
     // Calculations required for driving motor conversion factors and feed forward
     public static final double kDrivingMotorFreeSpeedRps = NeoMotorConstants.kFreeSpeedRpm / 60;
