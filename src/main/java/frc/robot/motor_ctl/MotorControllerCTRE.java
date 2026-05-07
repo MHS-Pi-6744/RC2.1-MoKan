94edea526da88438c01a200610e803fcde34f4a5
Lupeydev <197533886+Lupeydev@users.noreply.github.com>
2026-05-07 15:30:30 -0500

    started working on motor controller

--- src/main/java/frc/robot/motor_ctl/MotorControllerCTRE.java
+++ src/main/java/frc/robot/motor_ctl/MotorControllerCTRE.java
@@ -1,5 +1,18 @@
 package frc.robot.motor_ctl;
 
+import com.ctre.phoenix6.controls.PositionDutyCycle;
+import com.ctre.phoenix6.hardware.TalonFXS;
+
 public class MotorControllerCTRE {
-    
+    private String s_motorName;
+    private TalonFXS m_otor;
+
+    public MotorControllerCTRE(int canID){
+        s_motorName = "Motor #" + canID + "/";
+        m_otor = new TalonFXS(canID);
+
+        var position_request = new PositionDutyCycle(pos);
+
+        m_otor.setControl(position_request);
+    }
 }
