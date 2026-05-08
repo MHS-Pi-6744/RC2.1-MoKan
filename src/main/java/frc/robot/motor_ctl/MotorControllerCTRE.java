7ab0259ebb19c496dcf8ef925c1524fa3b17e1b0
Lupeydev <197533886+Lupeydev@users.noreply.github.com>
2026-05-08 15:54:11 -0500

    i made some quick easy commands

--- src/main/java/frc/robot/motor_ctl/MotorControllerCTRE.java
+++ src/main/java/frc/robot/motor_ctl/MotorControllerCTRE.java
@@ -3,16 +3,30 @@ package frc.robot.motor_ctl;
 import com.ctre.phoenix6.controls.PositionDutyCycle;
 import com.ctre.phoenix6.hardware.TalonFXS;
 
-public class MotorControllerCTRE {
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class MotorControllerCTRE extends SubsystemBase {
     private String s_motorName;
     private TalonFXS m_otor;
 
     public MotorControllerCTRE(int canID){
         s_motorName = "Motor #" + canID + "/";
         m_otor = new TalonFXS(canID);
-
-        var position_request = new PositionDutyCycle(pos);
-
-        m_otor.setControl(position_request);
     }
+    /**
+     * @return a {@link Command} that sets the motor speed to nothing
+     */
+    public Command stopMotor(double speed) {
+        return runOnce(() -> m_otor.set(0));
+    }
+
+      /**
+   * @param speed the speed the motor should go
+   * @return a {@link Command} that sets the motor speed to the input speed
+   */
+    public Command runMotor(double speed) {
+        return runOnce(() -> m_otor.set(speed));
+    }
+
 }
